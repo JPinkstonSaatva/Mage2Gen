2d6f246b16d0f3f2f755fa713a035f701263e703
Jeff Pinkston <jpinkston@saatvamattress.com>
2021-02-15 17:47:49 -0600

    Payment updates

--- mage2gen/snippets/payment.py
+++ mage2gen/snippets/payment.py
@@ -25,8 +25,10 @@ class PaymentSnippet(Snippet):
 
 	Generated Payment methods can be found in *Magento Adminpanel > Stores > Settings > Configuration > Sales > Payment Methods*
 
-	It allows you to add extra payment methods to Magento. For example if you need to have a payment method which can only be used in the backend 
+	It allows you to add extra payment methods to Magento. For example if you need to have a payment method which can only be used in the backend
 	or if you need a payment that directly creates an invoice.
+
+	Input for the payment method
 
 	"""
 
@@ -36,10 +38,12 @@ class PaymentSnippet(Snippet):
 		payment_class_name = method_name
 
 		payment_class = Phpclass('Model\\Payment\\'+payment_class_name,
-			extends='\Magento\Payment\Model\Method\AbstractMethod',
+			extends='\Magento\Payment\Model\Method\Cc',
 			attributes=[
 				'protected $_code = "'+payment_code+'";',
-				'protected $_isOffline = true;'
+				'protected $_isOffline = true;',
+				'protected $_canAuthorize = true;',
+				'protected $_canCapture = true;'
 			])
 
 		payment_class.add_method(Phpmethod(
@@ -47,27 +51,148 @@ class PaymentSnippet(Snippet):
 			params=[
 				'\\Magento\\Quote\\Api\\Data\\CartInterface $quote = null'
 			],
-			body="return parent::isAvailable($quote);" 
+			body="return parent::isAvailable($quote);"
 		))
-	
-		self.add_class(payment_class)
 
-		payment_file = 'etc/payment.xml'
+		payment_class.add_method(Phpmethod(
+			'capture',
+			params=[
+				'\\Magento\\Payment\\Model\\InfoInterface $payment',
+				'$amount'
+			],
+			body="""
+				try {
+					//check if payment has been authorized
+					if(is_null($payment->getParentTransactionId())) {
+						$this->authorize($payment, $amount);
+					}
 
-		payment_xml = Xmlnode('payment',attributes={'xmlns:xsi':"http://www.w3.org/2001/XMLSchema-instance","xsi:noNamespaceSchemaLocation":"urn:magento:module:Magento_Payment:etc/payment.xsd"},nodes=[
-			Xmlnode('groups',nodes=[
-				Xmlnode('group',attributes={'id':'offline'}, nodes=[
-					Xmlnode('label',node_text='Offline Payment Methods')
-				])
-			]),
-			Xmlnode('methods',nodes=[
-				Xmlnode('method',attributes={'name':payment_code},nodes=[
-					Xmlnode('allow_multiple_address',node_text='1'),
+					//build array of payment data for API request.
+					$request = [
+						'capture_amount' => $amount,
+						//any other fields, api key, etc.
+					];
+
+					//make API request to credit card processor.
+					$response = $this->makeCaptureRequest($request);
+
+					//todo handle response
+
+					//transaction is done.
+					$payment->setIsTransactionClosed(1);
+
+				} catch (\Exception $e) {
+					$message = $e->getMessage();
+					$this->debug($payment->getData(), $message);
+				}
+
+				return $this;
+					"""
+		))
+
+		payment_class.add_method(Phpmethod(
+			'authorize',
+			params=[
+				'\\Magento\\Payment\\Model\\InfoInterface $payment',
+				'$amount'
+			],
+			body="""
+				try {
+
+					///build array of payment data for API request.
+					$request = [
+						'cc_type' => $payment->getCcType(),
+						'cc_exp_month' => $payment->getCcExpMonth(),
+						'cc_exp_year' => $payment->getCcExpYear(),
+						'cc_number' => $payment->getCcNumberEnc(),
+						'amount' => $amount
+					];
+
+					//check if payment has been authorized
+					$response = $this->makeAuthRequest($request);
+
+				} catch (\Exception $e) {
+					$message = $e->getMessage();
+					$this->debug($payment->getData(), $message);
+				}
+
+				if(isset($response['transactionID'])) {
+					// Successful auth request.
+					// Set the transaction id on the payment so the capture request knows auth has happened.
+					$payment->setTransactionId($response['transactionID']);
+					$payment->setParentTransactionId($response['transactionID']);
+				}
+
+				//processing is not done yet.
+				$payment->setIsTransactionClosed(0);
+
+				return $this;
+		"""
+		))
+
+		payment_class.add_method(Phpmethod(
+			'getConfigPaymentAction',
+			body='return self::ACTION_AUTHORIZE_CAPTURE;'
+		))
+
+		payment_class.add_method(Phpmethod(
+			'makeAuthRequest',
+			params=['$request'],
+			body="""
+				$response = ['transactionId' => 123]; //todo implement API call for auth request.
+
+				if(!$response) {
+					throw new \Magento\Framework\Exception\LocalizedException(__('Failed auth request.'));
+				}
+
+				return $response;
+			"""
+		))
+
+		payment_class.add_method(Phpmethod(
+			'makeCaptureRequest',
+			params=['$request'],
+			body="""
+				$response = ['success']; //todo implement API call for capture request.
+
+				if(!$response) {
+					throw new \Magento\Framework\Exception\LocalizedException(__('Failed capture request.'));
+				}
+
+				return $response;
+			"""
+		))
+
+		# self.add_class(payment_class)
+
+		# payment_file = 'etc/payment.xml'
+
+		# payment_xml = Xmlnode('payment',attributes={'xmlns:xsi':"http://www.w3.org/2001/XMLSchema-instance","xsi:noNamespaceSchemaLocation":"urn:magento:module:Magento_Payment:etc/payment.xsd"},nodes=[
+		# 	Xmlnode('groups',nodes=[
+		# 		Xmlnode('group',attributes={'id':'offline'}, nodes=[
+		# 			Xmlnode('label',node_text='Offline Payment Methods')
+		# 		])
+		# 	]),
+		# 	Xmlnode('methods',nodes=[
+		# 		Xmlnode('method',attributes={'name':payment_code},nodes=[
+		# 			Xmlnode('allow_multiple_address',node_text='1'),
+		# 		])
+		# 	])
+		# ]);
+
+		# self.add_xml(payment_file, payment_xml)
+
+		di_file = 'etc/adminhtml/di.xml'
+
+		di = Xmlnode('config', attributes={'xsi:noNamespaceSchemaLocation':"urn:magento:framework:ObjectManager/etc/config.xsd"},nodes=[
+			Xmlnode('type', attributes={'name':'Magento\Payment\Model\CcGenericConfigProvider'},nodes=[
+				Xmlnode('arguments',nodes=[
+					Xmlnode('argument',attributes={'name':'methodCodes','xsi:type':'array'},nodes=[
+						Xmlnode('item',attributes={'name':payment_code,'xsi:type':'const'},node_text=payment_class.class_namespace + '::CODE')
+					])
 				])
 			])
-		]);
-
-		self.add_xml(payment_file, payment_xml)
+		])
 
 		config_file = 'etc/config.xml'
 
@@ -81,6 +206,7 @@ class PaymentSnippet(Snippet):
 						Xmlnode('title',node_text=method_name),
 						Xmlnode('allowspecific',node_text='0'),
 						Xmlnode('group',node_text='offline'),
+						Xmlnode('cctypes', node_text='AE,VI,MC,DI')
 					])
 				])
 			])
@@ -92,91 +218,37 @@ class PaymentSnippet(Snippet):
 
 		system = Xmlnode('config', attributes={'xsi:noNamespaceSchemaLocation':"urn:magento:module:Magento_Config:etc/system_file.xsd"}, nodes=[
 				Xmlnode('system',  nodes=[
-					Xmlnode('section',attributes={'id':'payment','sortOrder':1000,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
-						Xmlnode('group', attributes={'id':payment_code,'sortOrder':10,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+					Xmlnode('section',attributes={'id':'payment','sortOrder':1000,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+						Xmlnode('group', attributes={'id':payment_code,'sortOrder':10,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 							Xmlnode('label',node_text=method_name),
-							Xmlnode('field', attributes={'id':'active','type':'select','sortOrder':10,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+							Xmlnode('field', attributes={'id':'active','type':'select','sortOrder':10,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 								Xmlnode('label',node_text='Enabled'),
 								Xmlnode('source_model',node_text='Magento\\Config\\Model\\Config\\Source\\Yesno'),
 							]),
-							Xmlnode('field', attributes={'id':'title','type':'text','sortOrder':20,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+							Xmlnode('field', attributes={'id':'title','type':'text','sortOrder':20,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 								Xmlnode('label',node_text='Title'),
 							]),
-							Xmlnode('field', attributes={'id':'order_status','type':'select','sortOrder':30,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+							Xmlnode('field', attributes={'id':'order_status','type':'select','sortOrder':30,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 								Xmlnode('label',node_text='New Order Status'),
 								Xmlnode('source_model',node_text='Magento\\Sales\\Model\\Config\\Source\\Order\\Status\\NewStatus'),
 							]),
-							Xmlnode('field', attributes={'id':'allowspecific','type':'allowspecific','sortOrder':40,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
-								Xmlnode('label',node_text='Payment from Applicable Countries'),
-								Xmlnode('source_model',node_text='Magento\\Payment\\Model\Config\\Source\\Allspecificcountries'),
-							]),
-							Xmlnode('field', attributes={'id':'specificcountry','type':'multiselect','sortOrder':50,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
-								Xmlnode('label',node_text='Payment from Applicable Countries'),
-								Xmlnode('source_model',node_text='Magento\\Directory\\Model\\Config\\Source\\Country'),
-								Xmlnode('can_be_empty',node_text='1'),
-							]),
-							Xmlnode('field', attributes={'id':'sort_order','type':'text','sortOrder':60,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+							Xmlnode('field', attributes={'id':'sort_order','type':'text','sortOrder':60,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 								Xmlnode('label',node_text='Sort Order'),
 							]),
-							Xmlnode('field', attributes={'id':'instructions','type':'textarea','sortOrder':70,'showInWebsite':1,'showInStore':1,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+							Xmlnode('field', attributes={'id':'instructions','type':'textarea','sortOrder':70,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
 								Xmlnode('label',node_text='Instructions'),
 							]),
-						])	
+							Xmlnode('cctypes', attributes={'id':'cctypes', 'type':'multiselect', 'sortOrder':65,'showInDefault':1,'translate':'label'},match_attributes={'id'},nodes=[
+								Xmlnode('label',node_text='Credit Card Types'),
+								Xmlnode('source_model',node_text='Magento\Payment\Model\Source\Cctype'),
+							]),
+						])
 					])
 				])
 		])
 
 		self.add_xml(system_file,system)
 
-		layout_file = 'view/frontend/layout/checkout_index_index.xml'
-
-		layout_payment = Xmlnode('item',attributes={'name':payment_code,'xsi:type':'array'},nodes=[
-		 	Xmlnode('item',attributes={'name':'component','xsi:type':'string'},node_text='{}/js/view/payment/{}'.format(self.module_name,payment_code)), 
-			Xmlnode('item',attributes={'name':'methods','xsi:type':'array'},nodes=[
-				Xmlnode('item',attributes={'name':payment_code,'xsi:type':'array'},nodes=[
-					Xmlnode('item',attributes={'name':'isBillingAddressRequired','xsi:type':'boolean'},node_text='true')
-				])
-			])
-		])
-
-		layout_item = Xmlnode('item',attributes={'name':'components','xsi:type':'array'},nodes=[
-			Xmlnode('item',attributes={'name':'checkout','xsi:type':'array'},nodes=[
-				Xmlnode('item',attributes={'name':'children','xsi:type':'array'},nodes=[
-					Xmlnode('item',attributes={'name':'steps','xsi:type':'array'},nodes=[
-						Xmlnode('item',attributes={'name':'children','xsi:type':'array'},nodes=[
-							Xmlnode('item',attributes={'name':'billing-step','xsi:type':'array'},nodes=[
-								Xmlnode('item',attributes={'name':'children','xsi:type':'array'},nodes=[
-									Xmlnode('item',attributes={'name':'payment','xsi:type':'array'},nodes=[
-										Xmlnode('item',attributes={'name':'children','xsi:type':'array'},nodes=[
-											Xmlnode('item',attributes={'name':'renders','xsi:type':'array'},nodes=[
-												Xmlnode('item',attributes={'name':'children','xsi:type':'array'},nodes=[layout_payment])
-											])
-										])
-									])
-								])
-							])
-						])
-					])
-				])
-			])
-		])
-
-		layout_base = Xmlnode('page', attributes={'xmlns:xsi':"http://www.w3.org/2001/XMLSchema-instance",'layout':'1column', 'xsi:noNamespaceSchemaLocation':'urn:magento:framework:View/Layout/etc/page_configuration.xsd'}, nodes=[
-			Xmlnode('body',nodes=[
-				Xmlnode('referenceBlock',attributes={'name':'checkout.root'},nodes=[
-					Xmlnode('arguments',nodes=[
-						Xmlnode('argument',attributes={'name':'jsLayout','xsi:type':'array'},nodes=[layout_item])
-					])
-				])
-			])
-		])
-
-
-		self.add_xml(layout_file,layout_base)
-
-		self.add_static_file('view/frontend/web/template/payment', StaticFile(payment_code + '.html', template_file='payment/payment.tmpl',context_data={'module_name':self.module_name,'payment_code':payment_code}))
-		self.add_static_file('view/frontend/web/js/view/payment', StaticFile(payment_code + '.js', template_file='payment/payment-js.tmpl',context_data={'module_name':self.module_name,'payment_code':payment_code}))
-		self.add_static_file('view/frontend/web/js/view/payment/method-renderer', StaticFile(payment_code + '-method.js', template_file='payment/payment-method-js.tmpl',context_data={'module_name':self.module_name,'payment_code':payment_code}))
 
 		self.add_static_file(
 			'.',
@@ -189,9 +261,9 @@ class PaymentSnippet(Snippet):
 	def params(cls):
 		return [
 			SnippetParam(
-				name='method_name', 
-				required=True, 
+				name='method_name',
+				required=True,
 				description='Example: Invoice, Credits',
 				regex_validator= r'^[a-zA-Z]{1}\w+$',
 				error_message='Only alphanumeric and underscore characters are allowed, and need to start with a alphabetic character.'),
-		]	
+		]
